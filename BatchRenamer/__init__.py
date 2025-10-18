5f228515d9f15f27f663f0cc2775eed7e8805f21
Leo Malinen <107366630+leo-malinen@users.noreply.github.com>
2025-10-18 16:50:00 -0700

    Update __init__.py for rename

--- BatchRenamer/__init__.py
+++ BatchRenamer/__init__.py
@@ -1,50 +1,71 @@
-# Blender Add-on Template
-# Contributor(s): Aaron Powell (aaron@lunadigital.tv)
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# __init__.py
+# Batch Rename Helper Add-on
+# Contributor(s): Aaron Powell, Leo Malinen
 
 bl_info = {
-        "name": "My Awesome Add-on",
-        "description": "Single line describing my awesome add-on.",
-        "author": "Aaron Powell",
-        "version": (1, 0),
-        "blender": (2, 80, 0),
-        "location": "Properties > Render > My Awesome Panel",
-        "warning": "", # used for warning icon and text in add-ons panel
-        "wiki_url": "http://my.wiki.url",
-        "tracker_url": "http://my.bugtracker.url",
-        "support": "COMMUNITY",
-        "category": "Render"
-        }
+    "name": "Batch Rename Helper",
+    "description": "Batch rename selected objects with prefix, suffix, and numbering.",
+    "author": "Aaron Powell",
+    "version": (1, 0),
+    "blender": (2, 80, 0),
+    "location": "Properties > Render > Batch Rename Helper",
+    "warning": "",
+    "wiki_url": "http://my.wiki.url",
+    "tracker_url": "http://my.bugtracker.url",
+    "support": "COMMUNITY",
+    "category": "Object"
+}
 
 import bpy
 
-#
-# Add additional functions here
-#
+# Operator for renaming
+class OBJECT_OT_batch_rename(bpy.types.Operator):
+    bl_idname = "object.batch_rename"
+    bl_label = "Batch Rename Objects"
+    bl_description = "Rename selected objects with chosen settings"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        scene = context.scene
+        props = scene.batch_rename_props
+
+        selected_objects = context.selected_objects
+        if not selected_objects:
+            self.report({'WARNING'}, "No objects selected")
+            return {'CANCELLED'}
+
+        for i, obj in enumerate(selected_objects, start=props.start_number):
+            new_name = props.base_name
+
+            if props.use_prefix and props.prefix:
+                new_name = f"{props.prefix}_{new_name}"
+
+            if props.use_suffix and props.suffix:
+                new_name = f"{new_name}_{props.suffix}"
+
+            if props.use_numbering:
+                new_name = f"{new_name}_{i:0{props.padding}d}"
+
+            obj.name = new_name
+
+        return {'FINISHED'}
+
 
 def register():
     from . import properties
     from . import ui
     properties.register()
     ui.register()
+    bpy.utils.register_class(OBJECT_OT_batch_rename)
+
 
 def unregister():
     from . import properties
     from . import ui
-    properties.unregister()
+    bpy.utils.unregister_class(OBJECT_OT_batch_rename)
     ui.unregister()
+    properties.unregister()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     register()
