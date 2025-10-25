6246c4ec20960ca01c8fbb46b03a38a1f120f2a1
Leo Malinen <107366630+leo-malinen@users.noreply.github.com>
2025-10-24 18:45:36 -0700

    Update ui.py with initial changes

--- BatchRenamer/ui.py
+++ BatchRenamer/ui.py
@@ -1,38 +1,39 @@
-# Blender Add-on Template
-# Contributor(s): Aaron Powell (aaron@lunadigital.tv)
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-
 import bpy
 from bpy.types import Panel
 
-#
-# Add additional functions here
-#
 
-class MyPanel(Panel):
-    bl_label = 'My Awesome Panel'
+class OBJECT_PT_batch_rename(Panel):
+    bl_label = "Batch Rename Helper"
     bl_space_type = 'PROPERTIES'
-    bl_region_type= 'WINDOW'
+    bl_region_type = 'WINDOW'
     bl_context = 'render'
 
     def draw(self, context):
-        row = self.layout.row()
-        row.prop(context.scene, 'my_property')
+        layout = self.layout
+        props = context.scene.batch_rename_props
+
+        layout.prop(props, "base_name")
+
+        col = layout.column(align=True)
+        col.prop(props, "use_prefix")
+        if props.use_prefix:
+            col.prop(props, "prefix")
+
+        col.prop(props, "use_suffix")
+        if props.use_suffix:
+            col.prop(props, "suffix")
+
+        col.prop(props, "use_numbering")
+        if props.use_numbering:
+            col.prop(props, "start_number")
+            col.prop(props, "padding")
+
+        layout.operator("object.batch_rename", icon="OUTLINER_DATA_FONT")
+
 
 def register():
-    bpy.utils.register_class(MyPanel)
+    bpy.utils.register_class(OBJECT_PT_batch_rename)
+
 
 def unregister():
-    bpy.utils.unregister_class(MyPanel)
+    bpy.utils.unregister_class(OBJECT_PT_batch_rename)
