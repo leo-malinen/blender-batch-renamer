6e59fad52451adeb03e4aa751e231f3a97a77a07
Leo Malinen <107366630+leo-malinen@users.noreply.github.com>
2026-04-06 13:06:31 -0700

    Update ui.py with new changes

--- BatchRenamer/ui.py
+++ BatchRenamer/ui.py
@@ -1,39 +1,39 @@
 import bpy
-from bpy.types import Panel
 
-
-class OBJECT_PT_batch_rename(Panel):
-    bl_label = "Batch Rename Helper"
-    bl_space_type = 'PROPERTIES'
-    bl_region_type = 'WINDOW'
-    bl_context = 'render'
+class VIEW3D_PT_sleek_renamer(bpy.types.Panel):
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'Renamer'
+    bl_label = "Batch Rename"
 
     def draw(self, context):
         layout = self.layout
-        props = context.scene.batch_rename_props
-
-        layout.prop(props, "base_name")
-
-        col = layout.column(align=True)
-        col.prop(props, "use_prefix")
-        if props.use_prefix:
-            col.prop(props, "prefix")
-
-        col.prop(props, "use_suffix")
-        if props.use_suffix:
-            col.prop(props, "suffix")
-
-        col.prop(props, "use_numbering")
-        if props.use_numbering:
-            col.prop(props, "start_number")
-            col.prop(props, "padding")
-
-        layout.operator("object.batch_rename", icon="OUTLINER_DATA_FONT")
-
-
-def register():
-    bpy.utils.register_class(OBJECT_PT_batch_rename)
-
-
-def unregister():
-    bpy.utils.unregister_class(OBJECT_PT_batch_rename)
+        props = context.scene.sleek_renamer_props
+        
+        box = layout.box()
+        col = box.column(align=True)
+        col.label(text="Apply To:", icon='OUTLINER_OB_MESH')
+        col.prop(props, "target", text="")
+        
+        layout.separator()
+        
+        box = layout.box()
+        col = box.column(align=True)
+        col.label(text="Search & Replace:", icon='VIEWZOOM')
+        col.prop(props, "find_str", text="Find")
+        col.prop(props, "replace_str", text="Replace")
+        
+        layout.separator()
+        
+        box = layout.box()
+        col = box.column(align=True)
+        col.label(text="Naming Rules:", icon='ADD')
+        col.prop(props, "prefix", text="Prefix")
+        col.prop(props, "base_name", text="Base Name")
+        col.prop(props, "suffix", text="Suffix")
+        
+        layout.separator()
+        
+        row = layout.row()
+        row.scale_y = 1.5
+        row.operator("object.sleek_batch_rename", icon='CHECKMARK')
